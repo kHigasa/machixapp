c68ade07a1a782d07f53f3c0009df68e3fae5a15
kHigasa <kojihigasa@gmail.com>
2018-11-17 00:02:37 +0900

    Initial commit

---
+++ machixapp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  machixapp
+//
+//  Created by andre on 2018/11/16.
+//  Copyright © 2018 kHigasa. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
